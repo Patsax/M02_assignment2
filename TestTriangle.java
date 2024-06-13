0e79ccc89bc97d099bdb35227b7f91e6413297df
Patrick <patasax22@yahoo.com>
2024-06-13 16:42:31 -0400

    M02 assignment 2 done

---
+++ TestTriangle.java
@@ -0,0 +1,18 @@
+package javaDevelop.M02.M02_assignment2;
+
+/*test program that creates a Triangle object with
+* sides 1, 1.5, 1, sets color yellow and filled true*/
+public class TestTriangle {
+    public static void main(String[] args) {
+        //create an object of Triangle class
+        Triangle my = new Triangle(1,1.5,1,"yellow", true);
+        //Call method getArea() to get the area of Triangle
+        System.out.println("Area of Triangle " +my.getArea());
+        //Call method getPerimeter() to get the perimeter of Triangle
+        System.out.println("Perimeter of triangle " +my.getPerimeter());
+        //call getter method getColor()
+        System.out.println("color of the Triangle is " +my.getColor());
+        //call getter method isFilled()
+        System.out.println("is triangle filled " +my.isFilled());
+    }
+}
